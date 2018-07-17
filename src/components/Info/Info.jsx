3b60ce502632197b292f9b15365a161962fe26be
Ann Caly <anncaly@gmail.com>
2018-07-17 21:31:38 +0300

    tabs to spaces

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -9,19 +9,19 @@ export default class Info extends Component {
       <Fragment>
         <Header />
         <div className="pokemon_info">
-        	<div className="details">
+          <div className="details">
             <div className="details_img">
               <img
-      				  src={`https://raw.githubusercontent.com/epam-js-may-2018/homework-7-js/master/pokemons/${id}.png`}
-      				  width="100"
-      				  height="100"
+                src={`https://raw.githubusercontent.com/epam-js-may-2018/homework-7-js/master/pokemons/${id}.png`}
+                width="100"
+                height="100"
                 alt={title}
-      				/>
+              />
             </div>
             <div className="details_info">
               <p>ID: {id}</p>
               <h3>{title}</h3>
-      				<p>{date}</p>
+              <p>{date}</p>
             </div>
           </div>
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -15,20 +15,20 @@ import {
 } from 'react-router-dom';
 
 class App extends Component {
-	render(){
-		return(
-	    <div className="pokemons">
-  	    <BrowserRouter>
+  render(){
+    return(
+      <div className="pokemons">
+        <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Main}/>
-          	<Route exact path="/all_pokemons" component={PostsListContainer}/>
-        		<Route exact path="/pokemon/:id/" component={PokemonInfo}/>
-        		<Route exact path="/caught_pokemons" component={PokemonsCaughtList}/>
+            <Route exact path="/all_pokemons" component={PostsListContainer}/>
+            <Route exact path="/pokemon/:id/" component={PokemonInfo}/>
+            <Route exact path="/caught_pokemons" component={PokemonsCaughtList}/>
           </Switch>
         </BrowserRouter>
-	    </div>
-	  );
-	}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
